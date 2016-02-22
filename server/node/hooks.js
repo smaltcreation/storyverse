b222fcae9f6c0ae524e51a31226f15b1e02162bb
Samy Laumonier <159024@supinfo.com>
2016-02-22 02:02:47 +0100

    Continue the story

--- server/node/hooks.js
+++ server/node/hooks.js
@@ -9,6 +9,14 @@ Collection.Nodes.before.insert(function (userId, doc) {
 
     if (doc.from) {
         let node = Collection.Nodes.findOne(doc.from);
-        doc.root = node.root;
+        doc.root = node.root ? node.root : node._id;
     }
+
+    // Update user profile
+    Meteor.users.update(userId, {
+        $set: {
+            'profile.language': doc.language,
+            'profile.color': doc.color
+        }
+    });
 });
